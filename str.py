388ebe02aa98d82875b391865f8ca4c8874138af
NurkasymTynystan <n_tynystan@kbtu.kz>
2025-01-17 12:34:40 +0500

    Add files via upload

---
+++ str.py
@@ -0,0 +1,61 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+a = "Hello, World!"
+print(a[1:8]) # ello, Wo 
+
+
+
+
+
+
+
+for x in "banana":
+  print(x) # b a n a n a 
+
+
+  a = "Hello, World!"
+print(len(a)) # 13
+
+txt = "The best things in life are free!"
+print("free" in txt)  #true
+
+
+#slicing string 
+b = "Hello, World!"
+print(b[:5])
+
+
+#Modify string 
+
+a = "Hello, World!"
+print(a.upper()) # or print(a.lower()) 
+
+
+#f-string 
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+# We are the so-called "Vikings" from the north.
